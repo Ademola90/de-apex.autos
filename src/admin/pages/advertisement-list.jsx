fe1ebd0e984470d3da5b4751a66e5466a8335032
Ademola Abdullahi <130805693+Ademola90@users.noreply.github.com>
2025-04-15 15:58:47 +0100

    ads

--- src/admin/pages/advertisement-list.jsx
+++ src/admin/pages/advertisement-list.jsx
@@ -155,7 +155,7 @@ const AdvertisementList = () => {
             onClick={() => navigate("/admin/advertisement-form")}
             className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-mainBlue hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-mainBlue"
           >
-            <IoAdd className="mr-2" /> New Advertisement
+            <IoAdd className="mr-2" /> New Ads
           </button>
         </div>
 
